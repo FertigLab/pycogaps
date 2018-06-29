83fcd07401aaffee217e43803d3de09c3ec5343d
Tiger Gao <tigergao@jrtechsol.com>
2018-06-29 11:36:44 -0400

    Implemented module using boost python

---
+++ src/Cogapsmoduleboost.cpp
@@ -0,0 +1,76 @@
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+#include <numpy/numpyconfig.h>
+#include <numpy/ndarrayobject.h>
+#include "Rpackage/src/GapsDispatcher.h"
+#include "Rpackage/src/data_structures/Matrix.h"
+
+namespace p = boost::python;
+namespace np = boost::python::numpy;
+
+RowMatrix convertPyMatrix(const np::ndarray pymat)
+{
+    RowMatrix mat(pymat.shape(0), pymat.shape(1));
+    for (unsigned i = 0; i < pymat.shape(0); ++i)
+    {
+        for (unsigned j = 0; j < pymat.shape(1); ++j)
+        {
+            mat(i,j) = pymat[i][j];
+        }
+    }
+    return mat;
+}
+
+np::ndarray convertRowMatrix(const RowMatrix &rmat)
+{
+    p::tuple dims = p::make_tuple(rmat.nRow(), rmat.nCol());
+    np::ndarray pymat = np::empty(dims, np::dtype::get_builtin<float>())
+    for (unsigned i = 0; i < rmat.nRow(); ++i)
+    {
+        for (unsigned j = 0; j < rmat.nCol(); ++j)
+        {
+            pymat[i][j] = mat(i,j);
+        }
+    }
+
+    return pymat;
+}
+
+np::ndarray convertColMatrix(const ColMatrix &cmat)
+{
+    p::tuple dims = p::make_tuple(rmat.nRow(), rmat.nCol());
+    np::ndarray pymat = np::empty(dims, np::dtype::get_builtin<float>())
+    for (unsigned i = 0; i < rmat.nRow(); ++i)
+    {
+        for (unsigned j = 0; j < rmat.nCol(); ++j)
+        {
+            pymat[i][j] = mat(i,j);
+        }
+    }
+
+    return pymat;
+}
+
+p::tuple run(np::ndarray D, unsigned numPatterns, unsigned seed, unsigned nCores)
+{
+    Py_Initialize();
+    np::initialize();
+
+    RowMatrix convDMatrix = convertPyMatrix(D);
+
+    GapsDispatcher dispatcher(seed);
+    dispatcher.setNumPatterns(numPatterns);
+    dispatcher.setNumCoresPerSet(nCores);
+    dispatcher.loadData(convDMatrix);
+    GapsResult result(dispatcher.run());
+
+    np::ndarray AMatrix = convertColMatrix(result.Amean);
+    np::ndarray PMatrix = convertRowMatrix(result.Pmean);
+
+    return p::make_tuple(AMatrix, PMatrix);
+}
+
+p::BOOST_PYTHON_MODULE(Cogaps)
+{
+    def("run", run, "run Cogaps");
+}
