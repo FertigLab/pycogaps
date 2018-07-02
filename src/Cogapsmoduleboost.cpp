e37775172c59d9bb276cab3d8ac7328060613969
Student <tgao11@jhu.edu>
2018-07-02 11:24:31 -0400

    Push to migrate to new dev environment

--- src/Cogapsmoduleboost.cpp
+++ src/Cogapsmoduleboost.cpp
@@ -11,11 +11,11 @@ namespace np = boost::python::numpy;
 RowMatrix convertPyMatrix(const np::ndarray pymat)
 {
     RowMatrix mat(pymat.shape(0), pymat.shape(1));
-    for (unsigned i = 0; i < pymat.shape(0); ++i)
+    for (int i = 0; i < pymat.shape(0); ++i)
     {
-        for (unsigned j = 0; j < pymat.shape(1); ++j)
+        for (int j = 0; j < pymat.shape(1); ++j)
         {
-            mat(i,j) = pymat[i][j];
+            mat(i,j) = *pymat[i][j];
         }
     }
     return mat;
@@ -24,12 +24,12 @@ RowMatrix convertPyMatrix(const np::ndarray pymat)
 np::ndarray convertRowMatrix(const RowMatrix &rmat)
 {
     p::tuple dims = p::make_tuple(rmat.nRow(), rmat.nCol());
-    np::ndarray pymat = np::empty(dims, np::dtype::get_builtin<float>())
+    np::ndarray pymat = np::empty(dims, np::dtype::get_builtin<float>());
     for (unsigned i = 0; i < rmat.nRow(); ++i)
     {
         for (unsigned j = 0; j < rmat.nCol(); ++j)
         {
-            pymat[i][j] = mat(i,j);
+            pymat[i][j] = rmat(i,j);
         }
     }
 
@@ -38,13 +38,13 @@ np::ndarray convertRowMatrix(const RowMatrix &rmat)
 
 np::ndarray convertColMatrix(const ColMatrix &cmat)
 {
-    p::tuple dims = p::make_tuple(rmat.nRow(), rmat.nCol());
-    np::ndarray pymat = np::empty(dims, np::dtype::get_builtin<float>())
-    for (unsigned i = 0; i < rmat.nRow(); ++i)
+    p::tuple dims = p::make_tuple(cmat.nRow(), cmat.nCol());
+    np::ndarray pymat = np::empty(dims, np::dtype::get_builtin<float>());
+    for (unsigned i = 0; i < cmat.nRow(); ++i)
     {
-        for (unsigned j = 0; j < rmat.nCol(); ++j)
+        for (unsigned j = 0; j < cmat.nCol(); ++j)
         {
-            pymat[i][j] = mat(i,j);
+            pymat[i][j] = cmat(i,j);
         }
     }
 
@@ -70,7 +70,7 @@ p::tuple run(np::ndarray D, unsigned numPatterns, unsigned seed, unsigned nCores
     return p::make_tuple(AMatrix, PMatrix);
 }
 
-p::BOOST_PYTHON_MODULE(Cogaps)
+BOOST_PYTHON_MODULE(Cogaps)
 {
-    def("run", run, "run Cogaps");
+  p::def("run", run, "run Cogaps");
 }
