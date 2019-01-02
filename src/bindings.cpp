a6c0d66da35ad6ab5834a50759c73a04f8548ea1
Tom Sherman <tomsherman159@gmail.com>
2019-01-02 14:28:51 -0500

    updated readme

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -5,28 +5,6 @@
 
 namespace py = pybind11;
 
-int add(int i, int j)
-{
-    return i + j;
-}
-
-int multiply(float x, float y)
-{
-    return x * y;
-}
-
-int getRows(const std::string &path)
-{
-    FileParser fp(path);
-    return fp.nRow();
-}
-
-int getCols(const std::string &path)
-{
-    FileParser fp(path);
-    return fp.nCol();
-}
-
 float runCogaps(const std::string &path)
 {
     GapsParameters params(path);
@@ -38,9 +16,5 @@ float runCogaps(const std::string &path)
 PYBIND11_MODULE(cogaps, m)
 {
     m.doc() = "CoGAPS Python Package";
-    m.def("add", &add, "A function which adds two numbers");
-    m.def("multiply", &multiply, "A function which multiplies two numbers");
-    m.def("get_rows", &getRows, "Get number of rows in file");
-    m.def("get_cols", &getCols, "Get number of columns in file");
     m.def("runCogaps", &runCogaps, "Run CoGAPS Algorithm");
 }
