2e7a466796bedb5617d84aa7e1c3585d69a00d18
Jeanette Johnson <johnsonje197@gmail.com>
2021-06-23 18:15:53 -0400

    added get and set methods

--- CoGAPSParameters.py
+++ CoGAPSParameters.py
@@ -10,12 +10,13 @@ def current_milli_time():
 # constructor has default values for each parameter
 class CogapsParams(object):
     def __init__(self) -> object:
+        self.distributed = None
         self.nPatterns = 7
         self.nIterations = 50000
         self.alphaA = 0.01
         self.alphaP = 0.01
         self.maxGibbsMassA = 100
-        self.massGibbMassP = 100
+        self.maxGibbMassP = 100
         self.seed = current_milli_time()
         self.sparseOptimization = False
         self.cut = self.nPatterns
@@ -33,16 +34,73 @@ class CogapsParams(object):
         self.whichMatrixFixed = 'N'
         self.takePumpSamples = False
 
-    def __setParams__(self, whichParam, value):
-        if(whichParam == "nPatterns"):
-            self.nPatterns=value
-        elif(whichParam == "nIterations"):
+    def setParam(self, whichParam, value):
+        if whichParam == "nPatterns":
+            self.nPatterns = value
+        elif whichParam == "nIterations":
             self.nIterations = value
+        elif whichParam == "alpha":
+            self.alphaA = value
+            self.alphaP = value
+        elif whichParam == "maxGibbsMass":
+            self.maxGibbsMassA = value
+            self.maxGibbsMassP = value
+        elif whichParam in ("nSets", "cut", "minNS", "maxNS"):
+            print("please set \'", whichParam, "\' with setDistributedParams")
+            return
+        elif whichParam in ("samplingAnnotation", "samplingWeight"):
+            print("please set \'", whichParam, "\' with setAnnotationWeights")
+            return
+        elif whichParam in ("fixedPatterns", "whichMatrixFixed"):
+            print("please set \'", whichParam, "\' with setFixedPatterns")
+            return
+        elif whichParam == "nPatterns":
+            self.nPatterns = value
+            self.cut = min(self.cut, self.nPatterns)
+        elif whichParam == "distributed":
+            if value == "none":
+                self.distributed = None
+            else:
+                self.distributed = value
+        elif whichParam in "singleCell":
+            print(whichParam, " has been deprecated, this parameter will be ignored")
+            return
+        else:
+            setattr(self, whichParam, value)
         return self
 
-
-
-
-
-
-
+    def show(self):
+        print("-- Standard Parameters --\n")
+        print("nPatterns           ", self.nPatterns, "\n")
+        print("nIterations         ", self.nIterations, "\n")
+        print("seed                ", self.seed, "\n")
+        print("sparseOptimization  ", self.sparseOptimization, "\n")
+        if self.distributed is not None:
+            print("distributed         ", self.distributed, "\n")
+        print("\n")
+        print("-- Sparsity Parameters --\n")
+        if self.alphaA==self.alphaP:
+            print("alpha         ", self.alphaA, "\n")
+        else:
+            print("alphaA        ", self.alphaA, "\n")
+            print("alphaP        ", self.alphaP, "\n")
+        if self.maxGibbsMassA==self.maxGibbsMassP:
+            print("maxGibbsMass  ", self.maxGibbsMassA, "\n")
+        else:
+            print("maxGibbsMassA ", self.maxGibbsMassA, "\n")
+            print("maxGibbsMassP ", self.maxGibbsMassP, "\n")
+        if self.distributed is not None:
+            print("\n")
+            print("-- Distributed CoGAPS Parameters --", "\n")
+            print("nSets         ", self.nSets, "\n")
+            print("cut           ", self.cut, "\n")
+            print("minNS         ", self.minNS, "\n")
+            print("maxNS         ", self.maxNS, "\n")
+        if self.geneNames is not None:
+            print("\n")
+            print(len(self.geneNames), "gene names provided\n")
+            print("first gene name:", self.geneNames[1], "\n")
+        if self.sampleNames is not None:
+            print("\n")
+            print(len(self.sampleNames), "sample names provided\n")
+            print("first sample name:", self.sampleNames[1], "\n")
