2c59806a5395be8e8812c425db3449144ff0ee37
Jeanette Johnson <johnsonje197@gmail.com>
2021-06-25 12:54:43 -0400

    bind params object

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -10,22 +10,21 @@
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 namespace py = pybind11;
 
-float runCogaps(const std::string &path)
+
+float runCogaps(const std::string &path, GapsParameters params)
 {
     std::cout<<"1";
-    GapsParameters params(path);
     std::cout<<"2";
     GapsRandomState randState(params.seed);
     GapsResult result(gaps::run(path, params, std::string(), &randState));
     return result.meanChiSq;
-      std::cout<<"in the runcogaps fxn";
-      return 0.0;
+    std::cout<<"in the runcogaps fxn";
+    return 0.0;
 }
 
 int runCPPTests()
 {
     std::cout<<"in the cpp testing function";
-    //return run_catch_unit_tests();
     return 0;
 }
 
@@ -34,4 +33,36 @@ PYBIND11_MODULE(pycogaps, m)
     m.doc() = "CoGAPS Python Package";
     m.def("runCogaps", &runCogaps, "Run CoGAPS Algorithm");
     m.def("runCPPTests", &runCPPTests, "Run CoGAPS C++ Tests");
+    py::class_<GapsParameters>(m, "GapsParameters")
+        .def(py::init<const std::string &>())
+        .def("print", &GapsParameters::print)
+        .def_readwrite("checkpointOutFile", &GapsParameters::checkpointOutFile)
+        .def_readwrite("checkpointFile", &GapsParameters::checkpointFile)
+        .def_readwrite("seed", &GapsParameters::seed)
+        .def_readwrite("nGenes", &GapsParameters::nGenes)
+        .def_readwrite("nSamples", &GapsParameters::nSamples)
+        .def_readwrite("nPatterns", &GapsParameters::nPatterns)
+        .def_readwrite("nIterations", &GapsParameters::nIterations)
+        .def_readwrite("maxThreads", &GapsParameters::maxThreads)
+        .def_readwrite("outputFrequency", &GapsParameters::outputFrequency)
+        .def_readwrite("checkpointInterval", &GapsParameters::checkpointInterval)
+        .def_readwrite("snapshotFrequency", &GapsParameters::snapshotFrequency)
+        .def_readwrite("alphaA", &GapsParameters::alphaA)
+        .def_readwrite("alphaP", &GapsParameters::alphaP)
+        .def_readwrite("maxGibbsMassA", &GapsParameters::maxGibbsMassA)
+        .def_readwrite("pumpThreshold", &GapsParameters::pumpThreshold)
+        .def_readwrite("snapshotPhase", &GapsParameters::snapshotPhase)
+        .def_readwrite("useFixedPatterns", &GapsParameters::useFixedPatterns)
+        .def_readwrite("subsetData", &GapsParameters::subsetData)
+        .def_readwrite("useCheckPoint", &GapsParameters::useCheckPoint)
+        .def_readwrite("transposeData", &GapsParameters::transposeData)
+        .def_readwrite("printMessages", &GapsParameters::printMessages)
+        .def_readwrite("subsetGenes", &GapsParameters::subsetGenes)
+        .def_readwrite("printThreadUsage", &GapsParameters::printThreadUsage)
+        .def_readwrite("useSparseOptimization", &GapsParameters::useSparseOptimization)
+        .def_readwrite("takePumpSamples", &GapsParameters::takePumpSamples)
+        .def_readwrite("asynchronousUpdates", &GapsParameters::asynchronousUpdates)
+        .def_readwrite("whichMatrixFixed", &GapsParameters::whichMatrixFixed)
+        .def_readwrite("workerID", &GapsParameters::workerID)
+        .def_readwrite("runningDistributed", &GapsParameters::runningDistributed);
 }
--- testing.py
+++ testing.py
@@ -6,12 +6,22 @@ navigate to your pycogaps direcotry and run 'pip install .'
 '''
 import pycogaps
 import CoGAPSParameters
+path = "./data/GIST.csv"
 
 print("-- Testing python bindings --\n")
-pycogaps.runCogaps("./data/GIST.csv")
+# pycogaps.runCogaps("./data/GIST.csv")
 pycogaps.runCPPTests()
 
-print("-- Testing CogapsParams object --\n")
-params = CoGAPSParameters.CogapsParams()
-params.show()
+# print("-- Testing CogapsParams object --\n")
+# params = CoGAPSParameters.CogapsParams()
+# params.show()
 
+print("-- Passing params object into runCogaps function --\n")
+prm = pycogaps.GapsParameters(path)
+pycogaps.runCogaps(path, prm)
+
+
+
+# print("-- Making pho --\n")
+# largepho = pypho.pypho()
+# pycogaps.testStructCasting(largepho)
