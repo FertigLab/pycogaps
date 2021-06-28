bdace30ab372955b9a3a8ca2968425dc15cd5c66
Ashley Tsang <atsang5@jhu.edu>
2021-06-28 11:29:08 -0700

    added getDimNames, need to impl. allParams.gaps attribute

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,6 @@
 import pycogaps
 
+# we can use this for testing later 
 def getRetinaSubset(n=1):
     if not (1 <= n <= 4):
         raise Exception("invalide number of subsets requested")
@@ -35,3 +36,45 @@ def getSampleNames(data, transpose):
     if names == None or len(names) == 0:
         return ["Sample" + str(i) for i in range(1, ncolHelper(data))]
     return names
+
+def getDimNames(data, allParams):
+    geneNames = allParams.gaps.geneNames
+    sampleNames = allParams.gaps.sampleNames
+
+    if allParams.gaps.geneNames == None:
+        geneNames = getGeneNames(data, allParams.transposeData)
+    if allParams.gaps.sampleNames == None:
+        sampleNames = getSampleNames(data, allParams.transposeData)
+    
+    if allParams.transposeData:
+        nGenes = ncolHelper(data)
+        nSamples = nrowHelper(data)
+    else:
+        nGenes = nrowHelper(data)
+        nSamples = ncolHelper(data)
+
+    if allParams.gaps.subsetDim == 1:
+        nGenes = len(allParams.gaps.subsetIndices)
+        geneNames = geneNames[allParams.subsetIndices]
+    elif allParams.gaps.subsetDim == 2:
+        nSamples = len(allParams.subsetIndices)
+        sampleNames = sampleNames[allParams.subsetIndices]
+
+    if len(geneNames) != nGenes:
+        raise Exception(len(geneNames), " != ", nGenes, " incorrect number of gene names given")
+    if len(sampleNames) != nSamples:
+        raise Exception(len(sampleNames), " != ", nSamples, " incorrect number of sample names given")
+
+    # store processed gene/sample names directly in allParams list
+    # this is an important distinction - allParams@gaps contains the
+    # gene/sample names originally passed by the user, allParams contains
+    # the procseed gene/sample names to be used when labeling the result
+    allParams.geneNames <- geneNames
+    allParams.sampleNames <- sampleNames
+    return(allParams)
+
+##### testing #####
+
+path = "./data/GIST.csv"
+prm = pycogaps.GapsParameters(path)
+getDimNames(path, prm)
