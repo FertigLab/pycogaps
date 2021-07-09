f946ee77ec1e28cb1d379ff2b1c328098528a558
Ashley Tsang <atsang5@jhu.edu>
2021-07-09 10:57:14 -0700

    returns correct first array value

--- read_pydata.py
+++ read_pydata.py
@@ -13,14 +13,27 @@ adata_X = adata.X
 
 # mat = scipy.io.mmread('./data/GIST.mtx')
 
-# matrix can be returned as a np array
-# arr = np.array([[1,2],[3,4]], dtype=float)
-arr = np.ones((5,5), dtype=float)
-print(arr.dtype)
-# returns Matrix object
+print("\n######### Matrix from np array example ############")
+arr = np.array([[10,2],[3,4]], dtype=float)
+# arr = np.ones((2,2), dtype=float)*10
+print(arr)
 sample_mat = pycogaps.Matrix(arr)
-print(sample_mat.nRow())
-pycogaps.runCogapsFromMatrix(sample_mat, prm)
+np_ret = np.array(sample_mat, copy = False)
+print(np_ret)
+
+
+# print(sample_mat.nRow())
+# pycogaps.runCogapsFromMatrix(sample_mat, prm)
 
 # print(mat.shape)
 # pycogaps.runCogapsFromMatrix(mat, prm)
+
+print("\n######### Matrix from path example ############")
+matrix = pycogaps.Matrix(path, False, False, [])
+print(matrix.nRow())
+matrix_np = np.array(matrix, copy=False)
+print(matrix_np.shape)
+
+print("\n######### Random Matrix example ############")
+mat = pycogaps.Matrix(3,3)
+print(np.array(mat, copy=False))
