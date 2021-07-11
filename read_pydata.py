4e55346c352d0285c6af1a60fe5901444a532eab
Ashley Tsang <atsang5@jhu.edu>
2021-07-11 09:59:23 -0700

    wip, fills in matrix from only first element in arr

--- read_pydata.py
+++ read_pydata.py
@@ -5,22 +5,20 @@ import numpy as np
 
 # input is anndata object
 
-path = './data/GIST.mtx'
+path = './data/GIST.csv'
 prm = pycogaps.GapsParameters(path)
 
-adata = anndata.read_mtx(path)
+adata = anndata.read_csv(path)
 adata_X = adata.X
 
 # mat = scipy.io.mmread('./data/GIST.mtx')
 
 print("\n######### Matrix from np array example ############")
-arr = np.array([[10,2],[3,4]], dtype=float)
-# arr = np.ones((2,2), dtype=float)*10
-print(arr)
-sample_mat = pycogaps.Matrix(arr)
+sample_mat = pycogaps.Matrix(adata_X)
+print("finished calling constructor!\n")
 np_ret = np.array(sample_mat, copy = False)
+print("array to Matrix: \n")
 print(np_ret)
-
 
 # print(sample_mat.nRow())
 # pycogaps.runCogapsFromMatrix(sample_mat, prm)
@@ -28,12 +26,11 @@ print(np_ret)
 # print(mat.shape)
 # pycogaps.runCogapsFromMatrix(mat, prm)
 
-print("\n######### Matrix from path example ############")
-matrix = pycogaps.Matrix(path, False, False, [])
-print(matrix.nRow())
-matrix_np = np.array(matrix, copy=False)
-print(matrix_np.shape)
+# print("\n######### Matrix from path example ############")
+# matrix = pycogaps.Matrix(path, False, False, [])
+# matrix_np = np.array(matrix, copy=False)
+# print(matrix_np)
 
-print("\n######### Random Matrix example ############")
-mat = pycogaps.Matrix(3,3)
-print(np.array(mat, copy=False))
+# print("\n######### Random Matrix example ############")
+# mat = pycogaps.Matrix(3,3)
+# print(np.array(mat, copy=False))
