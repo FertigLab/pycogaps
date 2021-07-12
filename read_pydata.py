518df1a42c1895789a35963547a57d89265c5b83
Ashley Tsang <atsang5@jhu.edu>
2021-07-12 11:15:45 -0700

    supports np array and matrix obj conversions

--- read_pydata.py
+++ read_pydata.py
@@ -3,22 +3,30 @@ import pycogaps
 import scipy.io
 import scipy.sparse
 import numpy as np
+import PyCoGAPS
 
-# placeholder for when we have anndata samples
-path = './data/GIST.mtx'
+# placeholder until we have anndata samples
+path = './data/GIST.csv'
 prm = pycogaps.GapsParameters(path)
 
-adata = anndata.read_mtx(path)
+adata = anndata.read_csv(path)
 adata = adata.X
 
 if scipy.sparse.issparse(adata):
-    adata_X = adata.toarray()
+    adata = adata.toarray()
 
-sample_mat = pycogaps.Matrix(adata)
-np_ret = np.array(sample_mat, copy = False)
+matrix = pycogaps.Matrix(adata)
 
-print("~~~~~`pycogaps run from path: ~~~~~~~~~~\n")
-pycogaps.runCogaps(path, prm)
+print("\n~~~~~~~~~~~~ pycogaps run from path: ~~~~~~~~~~~~~~~\n")
+path_result = pycogaps.runCogaps(path, prm)
 
-print("~~~~~~pycogaps run from Matrix: ~~~~~~~~~~\n")
-pycogaps.runCogapsFromMatrix(sample_mat, prm)
+# converting Matrix object (GapsResult Amean) to numpy array
+path_Amean = np.array(path_result.Amean, copy = False)
+
+
+print("\n~~~~~~~~~~~~ pycogaps run from Matrix: ~~~~~~~~~~~~~~~~~\n")
+mat_result = pycogaps.runCogapsFromMatrix(matrix, prm)
+
+mat_Amean = np.array(mat_result.Amean, copy = False)
+
+
