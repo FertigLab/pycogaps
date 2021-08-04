aa77ed2ee3fb7bc722b384362d143f66ae76cc39
Ashley Tsang <atsang5@jhu.edu>
2021-08-03 22:38:40 -0700

    test checkpoints successful, fixed distributed param

--- test_checkpoints.py
+++ test_checkpoints.py
@@ -12,5 +12,5 @@ if isCheckpointsEnabled():
     res1 = res1['GapsResult']
     res2 = res2['GapsResult']
 
-    assert(np.isclose(toNumpy(res1.Amean), toNumpy(res2.Amean), rtol=0.1))
-    assert((np.allclose(toNumpy(res1.Pmean), toNumpy(res2.Pmean), rtol=0.1)))
+    assert(np.allclose(toNumpy(res1.Amean), toNumpy(res2.Amean), rtol=0.1))
+    assert(np.allclose(toNumpy(res1.Pmean), toNumpy(res2.Pmean), rtol=0.1))
