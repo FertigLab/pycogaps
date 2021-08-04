d5d8274305dbe7515414463c4e00224c72969cda
Ashley Tsang <atsang5@jhu.edu>
2021-08-03 22:09:25 -0700

    test checkpoints successful

---
+++ test_checkpoints.py
@@ -0,0 +1,16 @@
+from PyCoGAPS import *
+
+mtx_path = "./data/GIST.mtx" 
+
+if isCheckpointsEnabled():
+    params = CoParams(mtx_path)
+    setParams(params, {"nIterations": 100,
+                        "seed": 42})
+    res1 = CoGAPS(mtx_path, params, checkpointInterval=100, checkpointOutFile="test.out", messages=False)
+    res2 = CoGAPS(mtx_path, params, checkpointInFile="test.out", messages=False)
+
+    res1 = res1['GapsResult']
+    res2 = res2['GapsResult']
+
+    assert(np.isclose(toNumpy(res1.Amean), toNumpy(res2.Amean), rtol=0.1))
+    assert((np.allclose(toNumpy(res1.Pmean), toNumpy(res2.Pmean), rtol=0.1)))
