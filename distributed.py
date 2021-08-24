01150426335c75bcb6638f35547a034ac35847c0
Jeanette Johnson <johnsonje197@gmail.com>
2021-08-24 13:57:22 -0700

    wip

--- distributed.py
+++ distributed.py
@@ -11,7 +11,7 @@ def distributedCoGAPS(path, params, uncertainty=None):
     PyCoGAPS.setParams(params, {'checkpointOutFile':""})
     with multiprocessing.get_context("spawn").Pool(processes=len(sets)) as pool:
         m = PyCoGAPS.pycogaps.Matrix(4, 4)
-        result = pool.apply_async(callInternalCoGAPS, args=[path, params, 1, None, sets])
+        result = pool.apply_async(callInternalCoGAPS, args=[path, params, 1, sets, None])
         pool.close()
         print("closed the pool")
         pool.join()
@@ -19,7 +19,11 @@ def distributedCoGAPS(path, params, uncertainty=None):
     return result
 
 
-def callInternalCoGAPS(path, params, workerID, uncertainty=None, subsetIndices=None):
+def callInternalCoGAPS(path, params, workerID, subsetIndices=None, uncertainty=None):
+    if subsetIndices is None:
+        print("No subset indices provided; generating random sets...")
+        adata = helper_functions.toAnndata(path)
+        subsetIndices = subset_data.createSets(adata, params)
     if params.coparams['distributed'] == "genome-wide":
         genes = np.array(params.coparams['geneNames'])
         params.coparams['geneNames'] = np.take(genes, subsetIndices)
