f04a13302885f683b199c168f3a17888f6970801
Jeanette Johnson <johnsonje197@gmail.com>
2021-08-31 14:41:10 -0400

    map processes to runs

--- distributed.py
+++ distributed.py
@@ -1,3 +1,5 @@
+import warnings
+
 import PyCoGAPS
 import subset_data
 import multiprocessing
@@ -8,23 +10,42 @@ import numpy as np
 def distributedCoGAPS(path, params, uncertainty=None):
     data = helper_functions.toAnndata(path)
     sets = subset_data.createSets(data, params)
+    if min(map(len, sets)) < params.gaps.nPatterns:
+        warnings.warn("Data subset dimension less than nPatterns. Aborting.")
+        return 1
+
     PyCoGAPS.setParams(params, {'checkpointOutFile':""})
 
     if params.coparams["fixedPatterns"] is None:
         print("Running Across Subsets...\n\n")
         with multiprocessing.get_context("spawn").Pool(processes=len(sets)) as pool:
-            m = PyCoGAPS.pycogaps.Matrix(4, 4)
-            result = pool.apply_async(callInternalCoGAPS, args=[path, params, 1, sets, None])
+            # make a list of parameters for each function call so they can easily be mapped to processes
+            paramlst = []
+            for i in range(len(sets)):
+                paramlst.append([path, params, i, sets[i], None])
+
+            result = pool.map(callInternalCoGAPS, paramlst)
             pool.close()
             print("closed the pool")
             pool.join()
             print("joined the pool")
+            # if params.coparams['distributed'] == "genome-wide":
+            #     unmatched = result.get().Pmean
+            # else:
+            #     unmatched = result.get().Amean
+
     else:
         print("not implemented")
     return result
 
 
-def callInternalCoGAPS(path, params, workerID, subsetIndices=None, uncertainty=None):
+def callInternalCoGAPS(paramlst):
+    # take out parameters passed as a list to the worker process
+    path = paramlst[0]
+    params = paramlst[1]
+    workerID = paramlst[2]
+    subsetIndices = paramlst[3]
+    uncertainty = paramlst[4]
     if subsetIndices is None:
         print("No subset indices provided; generating random sets...")
         adata = helper_functions.toAnndata(path)
