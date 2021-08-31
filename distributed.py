d971db3e60f713c34b01e51342cf7f2a1febf77b
Jeanette Johnson <johnsonje197@gmail.com>
2021-08-31 18:21:48 -0400

    wip

--- distributed.py
+++ distributed.py
@@ -1,11 +1,14 @@
 import warnings
 
+from pandas.io.clipboard import paste
+
 import PyCoGAPS
 import subset_data
 import multiprocessing
 import helper_functions
 import numpy as np
-
+import pandas as pd
+import itertools
 
 def distributedCoGAPS(path, params, uncertainty=None):
     data = helper_functions.toAnndata(path)
@@ -14,7 +17,7 @@ def distributedCoGAPS(path, params, uncertainty=None):
         warnings.warn("Data subset dimension less than nPatterns. Aborting.")
         return 1
 
-    PyCoGAPS.setParams(params, {'checkpointOutFile':""})
+    PyCoGAPS.setParams(params, {'checkpointOutFile': ""})
 
     if params.coparams["fixedPatterns"] is None:
         print("Running Across Subsets...\n\n")
@@ -26,17 +29,45 @@ def distributedCoGAPS(path, params, uncertainty=None):
 
             result = pool.map(callInternalCoGAPS, paramlst)
             pool.close()
-            print("closed the pool")
             pool.join()
-            print("joined the pool")
-            # if params.coparams['distributed'] == "genome-wide":
-            #     unmatched = result.get().Pmean
-            # else:
-            #     unmatched = result.get().Amean
 
+            if params.coparams['distributed'] == "genome-wide":
+                # unmatched = list(map(lambda x: np.array(x["GapsResult"].Pmean), result))
+                unmatched = []
+                print("length of result", len(result))
+                for r in result:
+                    unmatched.append(np.array(r["GapsResult"].Pmean))
+            else:
+                unmatched = map(lambda x: np.array(x["GapsResult"].Amean), result)
+            print("Matching patterns across subsets...\n")
+            matched = findConsensusMatrix(unmatched, params)
     else:
-        print("not implemented")
-    return result
+        matched = params.gaps.fixedPatterns
+
+    return matched
+
+    # params.gaps.nPatterns = matched.consensus.shape[1]
+    # params.gaps.fixedPatterns = matched.consensus
+    # if params.coparams["distributed"] == "genome-wide":
+    #     params.gaps.whichMatrixFixed = "P"
+    # else:
+    #     params.gaps.whichMatrixFixed = "A"
+    #
+    # print("Running final stage...")
+    # with multiprocessing.get_context("spawn").Pool(processes=len(sets)) as pool:
+    #     paramlst = []
+    #     for i in range(len(sets)):
+    #         paramlst.append([path, params, i, sets[i], None])
+    #     finalresult = pool.map(callInternalCoGAPS, paramlst)
+    #     pool.close()
+    #     pool.join()
+    #
+    # fullresult = stitchTogether(finalresult, params, sets)
+    #
+    # # add diagnostics...
+    # fullresult.diagnostics.firstPass = result
+    #
+    # return fullresult
 
 
 def callInternalCoGAPS(paramlst):
@@ -68,5 +99,35 @@ def callInternalCoGAPS(paramlst):
     return gapsresult
 
 
-def callback(mat, params):
-    return "returned from callback"
+def findConsensusMatrix(unmatched, params):
+    print("not yet implemented")
+    allpatterns = pd.DataFrame(np.hstack(unmatched))
+    comb = expandgrid(range(params.coparams["nSets"]), range(params.gaps.nPatterns))
+    comb = list(comb.values())
+    comb = pd.DataFrame(comb)
+    comb = pd.DataFrame.transpose(comb)
+    comb = comb.to_numpy()
+    names = []
+    for i in range(comb.shape[0]):
+        names.append(str(comb[i, 0]+1) + "." + str(comb[i, 1]+1))
+    allpatterns.columns = names
+    print(allpatterns)
+    return patternMatch()
+    # allPatterns < - do.call(cbind, unmatchedPatterns)
+    # comb < - expand.grid(1: gapsParams @ nSets, 1: gapsParams @ nPatterns)
+    # colnames(allPatterns) < - paste(comb[, 1], comb[, 2], sep = ".")
+    # return (patternMatch(allPatterns, gapsParams))
+    return 1
+
+
+def expandgrid(*itrs):
+   product = list(itertools.product(*itrs))
+   return {'Var{}'.format(i+1):[x[i] for x in product] for i in range(len(itrs))}
+
+
+def patternMatch():
+    print("not yet implemented")
+
+
+def stitchTogether(result, params, sets):
+    print("not yet implemented")
