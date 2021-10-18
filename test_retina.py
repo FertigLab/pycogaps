118daab39932337717b9029215eaec8d2e89d20c
Ashley Tsang <atsang5@jhu.edu>
2021-10-18 01:22:01 -0400

    cleaning organized testing files

--- test_retina.py
+++
@@ -1,8 +0,0 @@
-from PyCoGAPS import *
-
-path = './src/CoGAPS/inst/extdata/retina_subset_1.h5'
-
-# if input is an hdf file, then need to create params object, and set key name when initializing
-params = CoParams(path, hdfKey='counts')
-
-result = CoGAPS(path, params=params)
