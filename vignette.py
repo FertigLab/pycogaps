f81418d8cbe6d68f2ee49e1b2e95c43cf4f7c9ba
Ashley Tsang <atsang5@jhu.edu>
2021-12-02 14:26:18 -0500

    changes to vignette for dockerfile

--- vignette.py
+++ vignette.py
@@ -9,34 +9,37 @@ print("This vignette was built using pycogaps version", getVersion())
 
 # The only required argument to CoGAPS is the path to the data.
 # This can be a .csv, .tsv, .mtx, .txt, .h5, or .h5ad file containing the data.
-path = "data/GSE98638_HCC.TCell.S5063.count.txt"
-table = pd.read_table(path)
-adata = anndata.AnnData(table.iloc[:, 2:])
-adata.obs_names = table["symbol"]
-sc.pp.log1p(adata)
-labelfile = "data/pheno.txt"
-table = pd.read_table(labelfile)
-majorCluster = table["majorCluster"]
+# path = "data/GSE98638_HCC.TCell.S5063.count.txt"
+path = "data/GIST.csv"
+# table = pd.read_table(path)
+# adata = anndata.AnnData(table.iloc[:, 2:])
+# adata.obs_names = table["symbol"]
+# sc.pp.log1p(adata)
+# labelfile = "data/pheno.txt"
+# table = pd.read_table(labelfile)
+# majorCluster = table["majorCluster"]
 # adata.var_names = majorCluster
 params = CoParams(path)
 
 setParams(params, {
-            'nIterations': 30000,
+            'nIterations': 1000,
             'seed': 42,
-            'nPatterns': 10,
+            'nPatterns': 3,
             'useSparseOptimization': True,
-            'distributed': 'genome-wide'
+            'distributed': None,
+            # 'checkpointOutFile': './data/checkpoint.out',
+            # 'checkpointInterval': 5
         })
 
 if __name__ == '__main__':
     start = time.time()
     # params.setDistributedParams()
-    result = CoGAPS(adata, params)
+    result = CoGAPS(path, params)
     end = time.time()
     print("TIME:", end - start)
-    print("Pickling...")
-    pickle.dump(result, open("./data/30kresult.pkl", "wb"))
-    print("Pickling complete!")
-    plot(result, groups=majorCluster)
+    # print("Pickling...")
+    # pickle.dump(result, open("./data/10result.pkl", "wb"))
+    # print("Pickling complete!")
+    # plot(result, groups=majorCluster)
 
 # unpickled = pickle.load(open("./data/testresult.pkl", "rb"))
