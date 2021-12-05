a7b35deb11845277df3a17e539f464c4aa42f1bf
Jeanette Johnson <jjohn450@jhmi.edu>
2021-12-05 18:38:38 -0500

    download dataset from s3

--- vignette.py
+++ vignette.py
@@ -1,49 +1,44 @@
-from PyCoGAPS import *
-import pickle
-import scanpy as sc
-print("This vignette was built using pycogaps version", getVersion())
-
-# Running CoGAPS with default parameters
-# replace with the path to your data, or use this provided example
-# of scRNAseq/TCRseq data from HCC patients undergoing immunotherapy
-
-# The only required argument to CoGAPS is the path to the data.
-# This can be a .csv, .tsv, .mtx, .txt, .h5, or .h5ad file containing the data.
-# path = "data/GSE98638_HCC.TCell.S5063.count.txt"
-path = "data/GIST.csv"
-# table = pd.read_table(path)
-# adata = anndata.AnnData(table.iloc[:, 2:])
-# adata.obs_names = table["symbol"]
-# sc.pp.log1p(adata)
-# labelfile = "data/pheno.txt"
-# table = pd.read_table(labelfile)
-# majorCluster = table["majorCluster"]
-# adata.var_names = majorCluster
-params = CoParams(path)
-
-setParams(params, {
-            'nIterations': 1000,
-            'seed': 42,
-            'nPatterns': 3,
-            'useSparseOptimization': True,
-            'distributed': None,
-            # 'checkpointOutFile': './data/checkpoint.out',
-            # 'checkpointInterval': 5
-        })
-
 if __name__ == '__main__':
+    from PyCoGAPS import *
+    import pickle
+    import scanpy as sc
+    import boto3
+    print("This vignette was built using pycogaps version", getVersion())
+    
+    s3 = boto3.client('s3')
+    with open('data/GSE98638_HCC.TCell.S5063.count.txt', 'wb') as f:
+        s3.download_fileobj('pycogaps', 'GSE98638_HCC.TCell.S5063.count.txt', f)
+
+    path = "data/GSE98638_HCC.TCell.S5063.count.txt"
+
+    table = pd.read_table(path)
+    adata = anndata.AnnData(table.iloc[:, 2:])
+    adata.obs_names = table["symbol"]
+    sc.pp.log1p(adata)
+
+    labelfile = "data/pheno.txt"
+    table = pd.read_table(labelfile)
+    majorCluster = table["majorCluster"]
+    adata.var_names = majorCluster
+
+    params = CoParams(path)
+
+    setParams(params, {
+        'nIterations': 50000,
+        'seed': 42,
+        'nPatterns': 10,
+        'useSparseOptimization': True,
+        'distributed': "genome-wide",
+    })
+
     start = time.time()
-    # params.setDistributedParams()
+    params.setDistributedParams()
     result = CoGAPS(path, params)
     end = time.time()
     print("TIME:", end - start)
+            
     print("Pickling...")
-    pickle.dump(result, open("./data/10result.pkl", "wb"))
+    pickle.dump(result, open("./data/50kresultfinal.pkl", "wb"))
     print("Pickling complete!")
-    # plot(result, groups=majorCluster)
-    import boto3
-    s3 = boto3.client('s3')
-    with open('./data/10result.pkl', 'rb') as data:
-        s3.upload_fileobj(data, 'pycogaps', 'pycogapsresult.txt')
-
-# unpickled = pickle.load(open("./data/testresult.pkl", "rb"))
+    with open('./data/50kresult.pkl', 'rb') as data:
+        s3.upload_fileobj(data, 'pycogaps', '50kresult.pkl')
