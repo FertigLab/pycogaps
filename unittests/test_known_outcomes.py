23adf3b7260f2a1b6d44d096a445984478598f12
Dmitrijs Lvovs <dmitrijs.lvovs@gmail.com>
2023-11-06 18:14:09 -0500

    new tests init & add toy data test

---
+++ unittests/test_known_outcomes.py
@@ -0,0 +1,28 @@
+import unittest
+from PyCoGAPS.parameters import *
+from PyCoGAPS.pycogaps_main import CoGAPS
+import scanpy as sc
+
+
+# Tests that run pycogaps with known conditions and compare run outcomes
+
+
+class ModSimDefaultCase(unittest.TestCase):
+    #runs on ModSim - the toy dataset
+    PATH = 'data/ModSimData.txt'
+
+    def setUp(self):
+        modsim = sc.read_text(self.PATH)
+        params = CoParams(path=self.PATH)
+        self.res = CoGAPS(modsim, params)
+
+    def test(self):
+        #both recorded based on 4e7702f
+        self.assertEqual(4.033180764186739, np.mean(self.res.obs))
+        self.assertEqual(0.2936586886479366, np.mean(self.res.var))
+
+
+if __name__=='__main__':
+    ModSimDefaultCase()
+        
+        
