4409aa41b4888d35afc35e8a22fc95e77c91c8e8
Dmitrijs Lvovs <dmitrijs.lvovs@gmail.com>
2023-11-07 17:28:47 -0500

    add distributed test

--- unittests/test_known_outcomes.py
+++ unittests/test_known_outcomes.py
@@ -3,6 +3,9 @@ from PyCoGAPS.parameters import *
 from PyCoGAPS.pycogaps_main import CoGAPS
 import scanpy as sc
 
+import logging as log
+
+
 
 # Tests that run pycogaps with known conditions and compare run outcomes
 
@@ -22,7 +25,24 @@ class ModSimDefaultCase(unittest.TestCase):
         self.assertEqual(0.2936586886479366, np.mean(self.res.var))
 
 
+class ModSimDistributedRuns(unittest.TestCase):
+    #runs on ModSim - the toy dataset
+    PATH = 'data/ModSimData.txt'
+
+    def setUp(self):
+        modsim = sc.read_text(self.PATH)
+        params = CoParams(path=self.PATH)
+        setParams(params, {'distributed': 'genome-wide'})
+        params.setDistributedParams(nSets=7)
+        self.res_dist = CoGAPS(modsim, params)
+
+    def test(self):
+        #dummy test to check that distributed has ran
+        return True
+
+
+
 if __name__=='__main__':
-    ModSimDefaultCase()
+    unittest.main()
         
         
